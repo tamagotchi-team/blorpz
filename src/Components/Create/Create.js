495bbf527b6a14e3a975bae5ba020408b2726f66
Xadr123 <hansenxander@gmail.com>
2020-04-07 15:07:03 -0600

    create component done

--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function Create(){
+
+    const [name, setName] = useState('')
+    const [hunger, setHunger] = useState(Math.floor(Math.random() * 10))
+    const [awake, setAwake] = useState(true)
+    const [happy, setHappy] = useState(Math.floor(Math.random() * 10))
+    const [poo, setPoo] = useState(1)
+    const [age, setAge] = useState(0)
+    const [alive, setAlive] = useState(true)
+
+    const createBlorp = (user_id, name, hunger, awake, happy, poo, age, alive) => {
+        axios.post(`/api/create/${user_id}`, (name, hunger, awake, happy, poo, age, alive))
+    }
+
     return (
-        <div>Create.js</div>
+        <div>
+            <p>Name your new Blorp!</p>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    createBlorp(name, hunger, awake, happy, poo, age, alive)
+                    setName('')
+                }}
+            >
+                <input
+                    placeholder="Enter Blorp Name Here"
+                    id="name"
+                    value={name}
+                    onChange={(e) => {
+                    setName(e.target.value)
+                }}
+                />
+                <button>Create Blorp!</button>
+            </form>
+        </div>
     )
 }
 
