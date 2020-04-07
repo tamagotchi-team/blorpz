8289402c86d7c488aafdf72ae094bbc0da62cdcd
Jessica Myer <36251741+oeddiemeddie@users.noreply.github.com>
2020-04-07 12:20:03 -0600

    merging

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import routes from './routes'
 import { withRouter } from 'react-router-dom'
 
-import Auth from './Components/Auth/Auth'
-import Landing from './Components/Landing/Landing'
 import Nav from './Components/Nav/Nav'
 
 
