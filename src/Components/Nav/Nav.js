9125be09478c8355caf26675801a23b3d3dbe94d
Grace <grace@MacBook-Pro.local>
2020-04-08 11:51:22 -0600

    Nav done

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,10 +1,38 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import userReducer from '../../ducks/userReducer'
 
-function Nav(){
+function Nav(props) {
+
+    const [dropdown, setDropdown] = useState(false)
+
+    const handleClick = () => {
+        setDropdown(!dropdown)
+    }
+
     return (
-        <div>Nav.js</div>
+        <div>
+
+            <img src={'https://vignette.wikia.nocookie.net/tamagotchi/images/3/32/Marutchi.png/revision/latest?cb=20130906113534'}
+                className="menu-button"
+                onClick={handleClick}
+                style={{height: "60px", width: "60px"}}
+            />
+            {dropdown ? (
+                <div className="nav-links">
+                    <Link to='/playground/:blorp'><button>Playground</button></Link>
+                    <Link to='/graveyard'><button>Graveyard</button></Link>
+                    <Link to='/'><button>Logout</button></Link>
+                </div>
+            ) : (
+                    null
+                )}
+
+        </div>
     )
 }
 
 export default Nav
+
+
