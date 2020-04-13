46d45d19b4920e8d0a81b44903ac230eab89bd33
Jessica Myer <36251741+oeddiemeddie@users.noreply.github.com>
2020-04-13 16:56:14 -0600

    testing push

---
+++ src/__tests__/createBlorp.test.js
@@ -0,0 +1,47 @@
+// import React from 'react'
+// import { render } from '@testing-library-react'
+import createBlorp from './functions'
+
+describe('createBlorpz unit tests', () => {
+    test('createBlorp creates blorp that is alive', () => {
+        expect(createBlorp.alive).toBe(true)
+    })
+    
+    // test('createBlorp creates blorp that is awake', () => {
+    
+    // })
+    
+    // test('createBlorp creates blorp with happy level greater than 0', () => {
+    
+    // })
+    
+    // test('createBlorp creates blorp with a happy level less than 10', () => {
+    
+    // })
+    
+    // test('createBlorp creates blorp with a Hunger level greater than 0', () => {
+    
+    // })
+    
+    // test('createBlorp creates blorp with a hunger level less than 10', () => {
+        
+    // })
+    
+    // test('createBlorp creates blorp with one poop', () => {
+    
+    // })
+    
+    // it('Adds a blorp to the blorpz array', () => {
+    
+    // })
+})
+// createBlorp
+
+
+
+
+// getBlorp
+
+// updateBlorp
+
+
