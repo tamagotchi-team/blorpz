ebc0889cd1d2acabadb5bfaca980f20d3233074d
Xadr123 <hansenxander@gmail.com>
2020-04-13 14:03:28 -0600

    sleep timer working

--- src/Components/Create/Create.js
+++ src/Components/Create/Create.js
@@ -6,7 +6,18 @@ import userReducer from '../../ducks/userReducer'
 
 function Create(props) {
 
-    let blorpPicture = ['https://vignette.wikia.nocookie.net/tamagotchi/images/1/11/Mametchi_anime_large.png/revision/latest?cb=20130920042009', 'https://vignette.wikia.nocookie.net/tamagotchi/images/2/2b/Kuchipatchi_anime.PNG/revision/latest/scale-to-width-down/350?cb=20110918052545', 'https://vignette.wikia.nocookie.net/tamagotchi/images/9/98/Mimitchi_anime.PNG/revision/latest?cb=20150118162441', 'https://vignette.wikia.nocookie.net/tamagotchi/images/2/23/Kiki_anime.png/revision/latest?cb=20150130225113', 'https://vignette.wikia.nocookie.net/tamagotchi/images/b/b8/Kuro.png/revision/latest?cb=20150131001626', 'https://vignette.wikia.nocookie.net/tamagotchi/images/3/33/Anime_spacytchi.PNG/revision/latest/scale-to-width-down/350?cb=20120502054708', 'https://vignette.wikia.nocookie.net/tamagotchi/images/8/8d/Lovelitchi_anime.PNG/revision/latest?cb=20170729032254', 'https://vignette.wikia.nocookie.net/tamagotchi/images/0/03/Himespetchi_anime.png/revision/latest?cb=20120131150427', 'https://vignette.wikia.nocookie.net/tamagotchi/images/7/7f/Orene_anime.png/revision/latest?cb=20150131004747', 'https://vignette.wikia.nocookie.net/tamagotchi/images/6/61/Neenetchi_anime_artwork.png/revision/latest/scale-to-width-down/180?cb=20141002020213']
+    let blorpPicture = [
+        'https://vignette.wikia.nocookie.net/tamagotchi/images/1/11/Mametchi_anime_large.png/revision/latest?cb=20130920042009', 
+        'https://vignette.wikia.nocookie.net/tamagotchi/images/2/2b/Kuchipatchi_anime.PNG/revision/latest/scale-to-width-down/350?cb=20110918052545', 
+        'https://vignette.wikia.nocookie.net/tamagotchi/images/9/98/Mimitchi_anime.PNG/revision/latest?cb=20150118162441', 
+        'https://vignette.wikia.nocookie.net/tamagotchi/images/2/23/Kiki_anime.png/revision/latest?cb=20150130225113',
+        'https://vignette.wikia.nocookie.net/tamagotchi/images/b/b8/Kuro.png/revision/latest?cb=20150131001626',
+        'https://vignette.wikia.nocookie.net/tamagotchi/images/3/33/Anime_spacytchi.PNG/revision/latest/scale-to-width-down/350?cb=20120502054708', 
+        'https://vignette.wikia.nocookie.net/tamagotchi/images/8/8d/Lovelitchi_anime.PNG/revision/latest?cb=20170729032254', 
+        'https://vignette.wikia.nocookie.net/tamagotchi/images/0/03/Himespetchi_anime.png/revision/latest?cb=20120131150427', 
+        'https://vignette.wikia.nocookie.net/tamagotchi/images/7/7f/Orene_anime.png/revision/latest?cb=20150131004747',
+        'https://vignette.wikia.nocookie.net/tamagotchi/images/6/61/Neenetchi_anime_artwork.png/revision/latest/scale-to-width-down/180?cb=20141002020213'
+    ]
 
     const user_id = props.userReducer.user.user_id
     const [name, setName] = useState('')
--- src/Components/Playground/Playground.js
+++ src/Components/Playground/Playground.js
@@ -31,28 +31,19 @@ function Playground(props) {
         "You toss a ball around with your Blorp.",
         "You tickle your Blorp.",
         "You play tag with your Blorp.",
+        "Your blorp is asleep. You decide not to wake him up.",
         "Your blorp does not want to play right now."
     ];
-<<<<<<< HEAD
     const feedActions = [
-      "You feed your Blorp a cookie.",
-      "You feed your Blorp a sandwich.",
-      "You feed your Blorp a slice of pizza.",
-      "You feed your Blorp a bagel.",
-      "You feed your Blorp some cereal.",
-      "You feed your Blorp some ice cream."
+        "You feed your Blorp a cookie.",
+        "You feed your Blorp a sandwich.",
+        "You feed your Blorp a slice of pizza.",
+        "You feed your Blorp a bagel.",
+        "You feed your Blorp some cereal.",
+        "You feed your Blorp some ice cream.",
+        "Your blorp is asleep. You decide not to wake him up.",
+        "Your blorp does not want to eat right now."
     ];
-=======
-
-    const feedActions = [
-        "You play hide and seek with your blorp.",
-        "You take your blorp on a walk.",
-        "You play pattycake with your Blorp.",
-        "You toss a ball around with your Blorp.",
-        "You tickle your Blorp.",
-        "You play tag with your Blorp.",
-        "Your blorp does not want to play right now."]
->>>>>>> master
     const [blorpz, setBlorpz] = useState([]);
     const [play, setPlay] = useState(
         Math.floor(Math.random() * playActions.length - 1)
@@ -94,30 +85,57 @@ function Playground(props) {
         setBlorpz([...tempBlorpz])
     }, 1000 * 60)
 
+    useInterval(() => {
+        let tempBlorpz = [...blorpz]
 
+        tempBlorpz.forEach((element) => {
+            if(element.awake === true) {
+                element.awake = false
+                console.log(element.awake)
+                setTimeout(() => {
+                    element.awake = true
+                    console.log(element.awake)
+                }, 1000 * 60 * 2)
+            }
+        })
 
-
-    useEffect(() => {
-
-    })
+        setBlorpz([...tempBlorpz])
+    }, 1000 * 60 * 5)
 
     const feedBlorp = (index) => {
-        console.log(blorpz[index].hunger);
-        blorpz[index].hunger = 10;
-        console.log(blorpz[index].hunger);
-        setFeed(Math.floor(Math.random() * feedActions.length - 1))
-        setFeedText(true)
-        setTimeout(() => {
-            setFeedText(false)
-        }, 1000 * 2);
-        setTimeout(() => {
-            setPoo(true);
-        }, 1000 * 30);
+        if(blorpz[index].awake === false) {
+            setFeed(feedActions.length - 2)
+            setFeedText(true)
+            setTimeout(() => {
+                setFeedText(false)
+            }, 1000 * 2);
+        } else if(blorpz[index].hunger >= 10) {
+            setFeed(feedActions.length - 1)
+            setFeedText(true)
+            setTimeout(() => {
+                setFeedText(false)
+            }, 1000 * 2);
+        } else {
+            blorpz[index].hunger = 10;
+            setFeed(Math.floor(Math.random() * feedActions.length - 2))
+            setFeedText(true)
+            setTimeout(() => {
+               setFeedText(false)
+            }, 1000 * 2);
+            setTimeout(() => {
+                setPoo(true);
+            }, 1000 * 30);
+        }
     };
 
     const playBlorp = (index) => {
-        console.log(blorpz[index].happy);
-        if (blorpz[index].happy >= 10) {
+        if(blorpz[index].awake === false) {
+            setPlay(playActions.length - 2)
+            setPlayText(true)
+            setTimeout(() => {
+                setPlayText(false)
+            }, 1000 * 2);
+        } else if (blorpz[index].happy >= 10) {
             setPlay(playActions.length - 1);
             // console.log(blorpz[index].happy);
             setPlayText(true);
@@ -127,7 +145,7 @@ function Playground(props) {
         } else {
             blorpz[index].happy += 2;
             // console.log(blorpz[index].happy);
-            setPlay(Math.floor(Math.random() * playActions.length - 1));
+            setPlay(Math.floor(Math.random() * playActions.length - 2));
             setPlayText(true);
             setTimeout(() => {
                 setPlayText(false);
